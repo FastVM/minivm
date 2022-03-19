1a9973181ded2636dbcf21ba5709788628b3013e
Shaw Summa <shawsumma@gmail.com>
2022-03-19 07:36:38 -0400

    tcc compat

--- vm/lib.h
+++ vm/lib.h
@@ -1,19 +1,23 @@
 
 #pragma once
 
-// MiniVM needs these three at all times
+#if defined(__SIZE_TYPE__)
 typedef __SIZE_TYPE__ size_t;
-typedef __PTRDIFF_TYPE__ ptrdiff_t;
+#else
+#include <stddef.h>
+#endif
 
+#if defined(__UINT8_TYPE__)
 typedef __UINT8_TYPE__ uint8_t;
-typedef __UINT16_TYPE__ uint16_t;
-typedef __UINT32_TYPE__ uint32_t;
-typedef __UINT64_TYPE__ uint64_t;
+#else
+#include <stdint.h>
+#endif
 
-typedef __INT8_TYPE__ int8_t;
-typedef __INT16_TYPE__ int16_t;
+#if defined(__INT32_TYPE__)
 typedef __INT32_TYPE__ int32_t;
-typedef __INT64_TYPE__ int64_t;
+#else
+#include <stdint.h>
+#endif
 
 #define NULL ((void *)0)
 
