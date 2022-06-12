56fc9110dee4ff5c8e3d2c9a1b86ac08bdedf6fc
Shaw Summa <shawsumma@gmail.com>
2022-06-11 21:28:00 -0400

    remove vm_str*

--- vm/lib.h
+++ vm/lib.h
@@ -8,33 +8,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static inline size_t vm_strlen(const char *str)
-{
-  size_t len = 0;
-  while (str[len] != '\0')
-  {
-    len += 1;
-  }
-  return len;
-}
-
-static inline int vm_streq(const char *str1, const char *str2)
-{
-  for (;;)
-  {
-    if (*str1 != *str2)
-    {
-      return 0;
-    }
-    if (*str1 == '\0')
-    {
-      return 1;
-    }
-    str1 += 1;
-    str2 += 1;
-  }
-}
-
 #define vm_malloc(size) (malloc(size))
 #define vm_alloc0(size) (calloc(size, 1))
 #define vm_realloc(ptr, size) (realloc(ptr, size))
