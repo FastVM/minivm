4fce895aa682c97330eb45ff332f9905469e9e9f
Shaw Summa <shawsumma@gmail.com>
2022-07-31 17:45:58 -0400

    faster memfib

--- vm/ir/be/jit.h
+++ vm/ir/be/jit.h
@@ -3,9 +3,6 @@
 
 #include "../ir.h"
 
-void vm_ir_be_jit_block_impl(void *vDst, void *vgc, void *vstore, vm_ir_block_t *block);
 void vm_ir_be_jit(size_t nops, vm_ir_block_t *blocks);
-
-#define vm_ir_be_jit_block(dst, gc, store, block) (vm_ir_be_jit_block_impl((void*) (dst), (void*) (gc), (void*) (store), block))
 
 #endif
