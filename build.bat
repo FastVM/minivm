b424c7facd8f9a170e2b82152d82de2d404100ab
Shaw Summa <shawsumma@gmail.com>
2022-09-24 19:38:44 -0400

    do not build lib file

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 if "%CC%"=="" ( set "CC=clang" )
 if not exist bin mkdir bin || exit /b %errorlevel%
-%CC% -fuse-ld=llvm-lib -Wno-deprecated-declarations -Oz vm/asm.c vm/gc.c vm/ir/build.c vm/ir/const.c vm/ir/info.c vm/ir/toir.c vm/ir/be/int3.c -static          -o "bin/libminivm.lib"  %* || exit /b %errorlevel%
+@REM %CC% -fuse-ld=llvm-lib -Wno-deprecated-declarations -Oz vm/asm.c vm/gc.c vm/ir/build.c vm/ir/const.c vm/ir/info.c vm/ir/toir.c vm/ir/be/int3.c -static          -o "bin/libminivm.lib"  %* || exit /b %errorlevel%
 %CC% -fuse-ld=lld      -Wno-deprecated-declarations -Oz vm/asm.c vm/gc.c vm/ir/build.c vm/ir/const.c vm/ir/info.c vm/ir/toir.c vm/ir/be/int3.c main/asm.c -flto -o "bin/minivm-asm.exe" %* || exit /b %errorlevel%
 %CC% -fuse-ld=lld      -Wno-deprecated-declarations -Oz vm/asm.c vm/gc.c vm/ir/build.c vm/ir/const.c vm/ir/info.c vm/ir/toir.c vm/ir/be/int3.c main/run.c -flto -o "bin/minivm-run.exe" %* || exit /b %errorlevel%
