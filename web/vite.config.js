01e9fd889a924142911364ff57e9d9b83e78ddc6
Shaw Summa <shawsumma@gmail.com>
2024-03-26 05:42:28 -0400

    package nicer

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,7 +18,7 @@ export default defineConfig({
         outDir: 'public',
         rollupOptions: {
             output: {
-                format: "es",
+                format: 'es',
             },
         },
     },
