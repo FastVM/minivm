4c97af9147555bb675a8b52f8e4609c8edca0cf7
Shaw Summa <shawsumma@gmail.com>
2024-04-12 05:36:37 -0400

    remove vite-wasm dep

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         svelte(),
-        wasm(),
         topLevelAwait(),
     ],
     base: './',
