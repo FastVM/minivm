ae18c036cff26eb98b8902812df3e0c6a9f077db
Shaw Summa <shawsumma@gmail.com>
2024-06-12 15:12:27 -0400

    bumpo

--- vm/backend/interp/interp.c
+++ vm/backend/interp/interp.c
@@ -1060,7 +1060,7 @@ vm_std_value_t vm_interp_block(vm_interp_t *interp, vm_std_value_t *regs, vm_int
                 break;
             }
             default: {
-                __builtin_trap();
+                __builtin_unreachable();
                 break;
             }
         }
@@ -1105,7 +1105,7 @@ vm_std_value_t vm_interp_block(vm_interp_t *interp, vm_std_value_t *regs, vm_int
                         goto call_closure_next_arg;
                     }
                     default: {
-                        __builtin_trap();
+                        __builtin_unreachable();
                     }
                 }
             call_closure_end:;
@@ -1132,7 +1132,7 @@ vm_std_value_t vm_interp_block(vm_interp_t *interp, vm_std_value_t *regs, vm_int
                         goto call_ffi_next_arg;
                     }
                     default: {
-                        __builtin_trap();
+                        __builtin_unreachable();
                     }
                 }
             call_ffi_end:;
